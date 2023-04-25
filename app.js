30cb59eccca3dc3474b56e840157565278423ae2
M0hitMehra <786hitmen@gmail.com>
2023-04-25 23:26:04 +0530

    secomd commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express  from "express";
 import { connectDB } from "./config/database.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 
 
 const app = express();
@@ -10,6 +10,12 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
+
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:['GET', 'POST', 'PUT', 'DELETE']
+}))
 
 //ROutes For APIs
 import course from "./routes/courseRoutes.js"
@@ -25,6 +31,9 @@ app.use("/api/v1",other)
 
 export default app
 
+app.get("/",(req,res)=>{
+    res.send(`<h1>SIte is working click <a href=${process.env.FRONTEND_URL} >here</a> to visit website </h1>`)
+})
 
 import ErrorMiddleware from "./middlewares/Error.js";
 app.use(ErrorMiddleware)
