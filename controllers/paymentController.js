fe0abc2807a8c514f21d2b1ec865a456760f3fea
M0hitMehra <786hitmen@gmail.com>
2023-04-30 01:01:32 +0530

    6 commit paymentRoute fix

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -60,6 +60,12 @@ export const paymentVerfication = catchAsyncError(async (req, res, next) => {
 
   await user.save();
 
+  
+  res.status(200).json({
+    success: true,
+    redirect :`${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
+  });
+  
   res.redirect(
     `${process.env.FRONTEND_URL}/paymentsuccess?reference=${razorpay_payment_id}`
   );
