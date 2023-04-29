c6d3b0b1394d93bcded219ea2a26f74ef25debe4
M0hitMehra <786hitmen@gmail.com>
2023-04-29 22:32:07 +0530

    5 commit paymentRoute fix

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,13 +1,13 @@
 import express  from "express";
 import { isAuthenticated } from "../middlewares/auth.js";
-import { buySubscription, cancelSubscripition, getRazorPayKey } from "../controllers/paymentController.js";
+import { buySubscription, cancelSubscripition, getRazorPayKey, paymentVerfication } from "../controllers/paymentController.js";
 
 const router = express.Router()
 // Buy
 router.route('/subscribe').get(isAuthenticated , buySubscription )
 
 // Verfiy
-router.route('/paymentverification').post(isAuthenticated , buySubscription )
+router.route('/paymentverification').post(isAuthenticated , paymentVerfication )
 
 //get razorpay key
 router.route('/razorpaykey').get( getRazorPayKey )
